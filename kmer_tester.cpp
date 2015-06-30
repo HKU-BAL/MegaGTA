5f0c9d4ad518cb30a00195acbe5fd606d02458ce
HuangYukun <jimmyhuang.hku@gmail.com>
2015-06-30 18:18:58 +0800

    implement hashset for kmers

--- kmer_tester.cpp
+++ kmer_tester.cpp
@@ -9,10 +9,6 @@
 
 int main(int argc, char **argv) {
 
-	// if (argc == 1) {
-	// 	fprintf(stderr, "Usage: %s <in.seq>\n", argv[0]);
-	// 	exit(1);
-	// }
 
 	Kmer<4, uint64_t> kmer1;
 	int seq1[150];
